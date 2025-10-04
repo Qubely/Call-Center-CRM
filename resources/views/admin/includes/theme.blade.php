3d1cec63c9cf338f9fe9a9c4a0bf94fbcd82e50f
Paris Bala <arianbala56@gmail.com>
2025-10-04 16:12:04 +0600

    initial commit

---
+++ resources/views/admin/includes/theme.blade.php
@@ -0,0 +1,47 @@
+{{-- Override any default styles here --}}
+<style>
+a {
+    text-decoration: none;
+}
+.bread-cum {
+    margin: 12px 0;
+}
+.page-title {
+    font-weight: 900;
+}
+.page-fragment-bar {
+     padding: 5px 0;
+}
+.page-fragment-bar * {
+    font-size: 16px;
+    font-weight: bold;
+}
+#sidebar-menu ul li a {
+    color: #000;
+}
+.table-list hr {
+    color: #000;
+}
+.form-control {
+    border: var(--bs-border-width) solid #d1d1d1;
+}
+.table-list table.dataTable.no-footer {
+    border-bottom: 1px solid #e7e7e7;
+}
+.table>:not(caption)>*>* {
+    color: #000;
+}
+.rounded-pill {
+    padding: 6px;
+}
+.account-pages .logo{
+    width: 320px;
+}
+
+.admin-logo-sm  {
+    width: 38px;
+}
+.admin-logo-lg {
+    width: 90%;
+}
+</style>
