8e82fce09ed51c5e12523931ed7612ce46f28b95
Paris Bala <arianbala56@gmail.com>
2025-10-17 18:24:52 +0600

    data libary status added

--- resources/lang/en/admin/layout/main-nav.php
+++ resources/lang/en/admin/layout/main-nav.php
@@ -8,7 +8,7 @@ return [
     'system.core.user_policy' => 'User Policies',
     'company' => 'Companies',
     'company.list' => 'Company Lists',
-    'data-library.menu' => 'Data LIbrary',
+    'data-library.menu' => 'Data Library',
     'data-library.country-list' => 'Countries',
     'data-library.time-zone' => 'Time Zones',
     'data-library.status' => 'System Status'
